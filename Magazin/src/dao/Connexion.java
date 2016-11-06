ce8a02d6eefa5b8af6c0734e6461ae51fc977938
najdayari <ayarinajd@live.com>
2016-11-06 19:00:29 +0100

    connexion + class produit vide

---
+++ Magazin/src/dao/Connexion.java
@@ -0,0 +1,36 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Connexion {
+	
+	Connection c=null ; 
+	private String url="jdbc:mysql://localhost:3306/magasin_en_ligne";
+	private String user="root";
+	private String password="admin";
+	private Statement st;
+	
+	public Statement getConnexion(){
+		
+try {
+			
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		try {
+			c=DriverManager.getConnection(url,user,password);
+			st =c.createStatement();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return st ;
+		
+		
+	}
+}
