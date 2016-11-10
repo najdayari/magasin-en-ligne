65f7fc420804dd6a517e46e448562703c1bb26fc
Wided <Wided@192.168.1.6>
2016-11-10 18:43:15 +0100

    interface ajout produits

--- Magazin/src/managedBean/ManagedBean.java
+++ Magazin/src/managedBean/ManagedBean.java
@@ -20,41 +20,41 @@ public class ManagedBean {
 	private Produit pr = new Produit();
 	private ProduitDao p = new ProduitDao();
 	private ArrayList<Produit> l = new ArrayList<Produit>();
-	private int qte=1;;
+	private int qte=1;
 	
-public ManagedBean() {
+	public ManagedBean() {
+			
+		}
+	
 		
-	}
-
-	
 	public int getQte() {
-	return qte;
-}
+			return qte;
+		}
 
 
-public void setQte(int qte) {
-	this.qte = qte;
-}
+	public void setQte(int qte) {
+		this.qte = qte;
+	}
 
 
 	public Produit getPr() {
-	return pr;
-}
-
-
-public void setPr(Produit pr) {
-	this.pr = pr;
-}
-
-
-	public ProduitDao getP() {
-	return p;
-}
-
-
-public void setP(ProduitDao p) {
-	this.p = p;
-}
+		return pr;
+	}
+	
+	
+	public void setPr(Produit pr) {
+		this.pr = pr;
+	}
+	
+	
+		public ProduitDao getP() {
+		return p;
+	}
+	
+	
+	public void setP(ProduitDao p) {
+		this.p = p;
+	}
 
 
 	public ArrayList<Produit> getL() {
@@ -74,5 +74,60 @@ public void setP(ProduitDao p) {
 		return "";
 	}
 	
+	public String insert()
+	{
+		if(p.insert(pr)==0)
+			{
+			FacesMessage msg=new FacesMessage(FacesMessage.SEVERITY_WARN,"Ajouter","Insertion non effetctuée");
+			RequestContext.getCurrentInstance().showMessageInDialog(msg);
+			
+			}
+		else
+		{
+			FacesMessage msg=new FacesMessage(FacesMessage.SEVERITY_WARN,"Ajouter","Insertion effetctuée");
+			RequestContext.getCurrentInstance().showMessageInDialog(msg);
+			
+			}
+		
+		return "mmm";
+		
+		
+	}
+	
+	public String update()
+	{
+		if(p.update(pr)==0)
+			{FacesMessage msg=new FacesMessage(FacesMessage.SEVERITY_WARN,"Modifier","Modification non effetctuée");
+			 RequestContext.getCurrentInstance().showMessageInDialog(msg);
+		}
+		else
+			{
+			FacesMessage msg=new FacesMessage(FacesMessage.SEVERITY_WARN,"Modifier","Modificatio non effetctuée");
+			RequestContext.getCurrentInstance().showMessageInDialog(msg);
+			}
+		
+		return "mmm";
+		
+		
+	}
+	
+	public String delete()
+	{
+		if(p.delete(pr)==0)
+		{
+			FacesMessage msg=new FacesMessage(FacesMessage.SEVERITY_WARN,"Supp","Suppression non effetctuée");
+			RequestContext.getCurrentInstance().showMessageInDialog(msg);
+		}
+			
+		else
+		{
+			FacesMessage msg=new FacesMessage(FacesMessage.SEVERITY_INFO,"Supp","Suppression  effetctuée");
+			RequestContext.getCurrentInstance().showMessageInDialog(msg);
+			
+		}
+		return "mmm";
+		
+		
+	}
 	
 }
