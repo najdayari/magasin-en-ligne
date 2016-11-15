27f81e5eba4c5ac2e6c5aa16e6bcf6e52b2ee6d2
najdayari <ayarinajd@live.com>
2016-11-15 09:35:41 +0100

    produit dao update

--- Magazin/src/dao/ProduitDao.java
+++ Magazin/src/dao/ProduitDao.java
@@ -64,8 +64,7 @@ public class ProduitDao {
 		int i = 0;
 	
 			 try {
-				
-				 
+
 		i=	s.executeUpdate("INSERT INTO lignecommande (`numcmd`, `numprod`, `qte`,`prixtotal`,`pointstotal`) VALUES ('1', "+ar.getNumProd()+", "+qte+","+ar.getPrixTTC()*qte+","+ar.getPointProd()*qte+");");
 			s.close();
 			 } catch (SQLException e) {
